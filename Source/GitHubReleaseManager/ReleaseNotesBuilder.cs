2b0766b6f4b89da6eadcd128a93dca56605f8b19
Gary Ewan Park <gep13@gep13.co.uk>
2015-03-11 22:05:36 +0000

    (build) Fixing InspectCode Problem
    - Removed unnecessary using statement

--- Source/GitHubReleaseManager/ReleaseNotesBuilder.cs
+++ Source/GitHubReleaseManager/ReleaseNotesBuilder.cs
@@ -10,7 +10,6 @@ namespace GitHubReleaseManager
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Globalization;
-    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
