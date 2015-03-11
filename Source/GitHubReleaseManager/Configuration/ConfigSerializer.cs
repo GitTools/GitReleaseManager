5c1b3d4dac20937f15eb19c2cec2296dbdb1c6f6
Gary Ewan Park <gep13@gep13.co.uk>
2015-03-11 21:16:07 +0000

    (GH-21) Updated WriteSample Method
    - To include example of all possible configuration

--- Source/GitHubReleaseManager/Configuration/ConfigSerializer.cs
+++ Source/GitHubReleaseManager/Configuration/ConfigSerializer.cs
@@ -40,7 +40,24 @@ namespace GitHubReleaseManager.Configuration
                 throw new ArgumentNullException("writer");
             }
 
-            writer.WriteLine(@"# export-regex: ### Where to get it(\r\n)*You can .*\)");
+            writer.WriteLine(@"# create:");
+            writer.WriteLine(@"#   include-footer: true | false");
+            writer.WriteLine(@"#   footer-heading: Where to get it");
+            writer.WriteLine(@"#   footer-content: You can download this release from [chocolatey](https://chocolatey.org/packages/chocolateyGUI/{milestone}");
+            writer.WriteLine(@"#   footer-includes-milestone: true | false");
+            writer.WriteLine(@"#   milestone-replace-text: '{milestone}'");
+            writer.WriteLine(@"# export:");
+            writer.WriteLine(@"#   include-created-date-in-title: true | false");
+            writer.WriteLine(@"#   created-date-string-format: MMMM dd, yyyy");
+            writer.WriteLine(@"#   perform-regex-removal: true | false");
+            writer.WriteLine(@"#   regex-text: '### Where to get it(\r\n)*You can .*\)'");
+            writer.WriteLine(@"#   multiline-regex: true | false");
+            writer.WriteLine(@"# issue-labels-include:");
+            writer.WriteLine(@"# - Bug");
+            writer.WriteLine(@"# - Feature");
+            writer.WriteLine(@"# - Improvement");
+            writer.WriteLine(@"# issue-labels-exclude:");
+            writer.WriteLine(@"# - Internal Refactoring");
         }
     }
 }
