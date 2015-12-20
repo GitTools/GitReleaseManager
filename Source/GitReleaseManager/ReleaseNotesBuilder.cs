3c401360d301290671d5501fc07d67862c75d22c
Gary Ewan Park <gep13@gep13.co.uk>
2015-12-20 21:25:14 +0000

    (GH-60) Correcting Code Analysis Errors

--- Source/GitReleaseManager/ReleaseNotesBuilder.cs
+++ Source/GitReleaseManager/ReleaseNotesBuilder.cs
@@ -102,7 +102,7 @@ namespace GitReleaseManager.Core
 
         private void CheckForValidLabels(Issue issue)
         {
-            var count = this.configuration.IssueLabelsInclude.Sum(issueLabel => issue.Labels.Count(l => l.Name.ToLower() == issueLabel.ToLower()));
+            var count = this.configuration.IssueLabelsInclude.Sum(issueLabel => issue.Labels.Count(l => l.Name.ToUpperInvariant() == issueLabel.ToUpperInvariant()));
 
             if (count != 1)
             {
