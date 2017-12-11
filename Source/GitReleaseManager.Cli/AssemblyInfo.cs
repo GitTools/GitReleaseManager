3983a5fb155d8fd0f7dd1b4efbfc8123b71c0448
Gary Ewan Park <gep13@gep13.co.uk>
2017-12-11 21:58:08 +0000

    (GH-96) First pass at adding Cake.Recipe
    - Still some work to be done here, but starting to take shape

--- Source/GitReleaseManager.Cli/AssemblyInfo.cs
+++ Source/GitReleaseManager.Cli/AssemblyInfo.cs
@@ -6,22 +6,12 @@
 
 using System;
 using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GitReleaseManager.Cli")]
-[assembly: AssemblyProduct("GitReleaseManager.Cli")]
 [assembly: AssemblyDescription("Create release notes in markdown given a GitHub milestone")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GitTools Contributors")]
-[assembly: AssemblyCopyright("Copyright 2015 - Present - GitTools Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en-GB")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
 
 [assembly: Guid("284D3742-6799-4916-AC21-CFDDD7EE6850")]
