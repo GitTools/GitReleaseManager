3990d1fa70c5652a9552ae97371088de48819308
AdmiringWorm <kim.nordmo@gmail.com>
2019-12-20 15:35:07 +0100

    (GH-179) Configure Serilog logging

--- Source/GitReleaseManager.Cli/Program.cs
+++ Source/GitReleaseManager.Cli/Program.cs
@@ -7,8 +7,6 @@
 namespace GitReleaseManager.Cli
 {
     using System;
-    using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
     using System.Net;
     using System.Reflection;
@@ -16,22 +14,23 @@ namespace GitReleaseManager.Cli
     using System.Threading.Tasks;
     using AutoMapper;
     using CommandLine;
+    using Destructurama;
     using GitReleaseManager.Cli.Options;
     using GitReleaseManager.Core;
     using GitReleaseManager.Core.Configuration;
     using GitReleaseManager.Core.Helpers;
-    using FileMode = System.IO.FileMode;
+    using Serilog;
+    using Serilog.Events;
+    using Serilog.Sinks.SystemConsole.Themes;
 
     public static class Program
     {
-        private static readonly StringBuilder _log = new StringBuilder();
         private static FileSystem _fileSystem;
         private static Config _configuration;
         private static IMapper _mapper;
         private static IVcsProvider _vcsProvider;
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Not required")]
-        private static Task<int> Main(string[] args)
+        private static async Task<int> Main(string[] args)
         {
             // Just add the TLS 1.2 protocol to the Service Point manager until
             // we've upgraded to latest Octokit.
@@ -41,20 +40,33 @@ namespace GitReleaseManager.Cli
 
             _mapper = AutoMapperConfiguration.Configure();
 
-            return Parser.Default.ParseArguments<CreateSubOptions, DiscardSubOptions, AddAssetSubOptions, CloseSubOptions, PublishSubOptions, ExportSubOptions, InitSubOptions, ShowConfigSubOptions, LabelSubOptions, OpenSubOptions>(args)
-                .WithParsed<BaseSubOptions>(CreateFiglet)
-                .MapResult(
-                (CreateSubOptions opts) => CreateReleaseAsync(opts),
-                (DiscardSubOptions opts) => DiscardReleaseAsync(opts),
-                (AddAssetSubOptions opts) => AddAssetsAsync(opts),
-                (CloseSubOptions opts) => CloseMilestoneAsync(opts),
-                (PublishSubOptions opts) => PublishReleaseAsync(opts),
-                (ExportSubOptions opts) => ExportReleasesAsync(opts),
-                (InitSubOptions opts) => CreateSampleConfigFileAsync(opts),
-                (ShowConfigSubOptions opts) => ShowConfigAsync(opts),
-                (LabelSubOptions opts) => CreateLabelsAsync(opts),
-                (OpenSubOptions opts) => OpenMilestoneAsync(opts),
-                errs => Task.FromResult(1));
+            try
+            {
+                return await Parser.Default.ParseArguments<CreateSubOptions, DiscardSubOptions, AddAssetSubOptions, CloseSubOptions, PublishSubOptions, ExportSubOptions, InitSubOptions, ShowConfigSubOptions, LabelSubOptions>(args)
+                    .WithParsed<BaseSubOptions>(CreateFiglet)
+                    .WithParsed<BaseSubOptions>(ConfigureLogging)
+                    .MapResult(
+                    (CreateSubOptions opts) => CreateReleaseAsync(opts),
+                    (DiscardSubOptions opts) => DiscardReleaseAsync(opts),
+                    (AddAssetSubOptions opts) => AddAssetsAsync(opts),
+                    (CloseSubOptions opts) => CloseMilestoneAsync(opts),
+                    (OpenSubOptions opts) => OpenMilestoneAsync(opts),
+                    (PublishSubOptions opts) => PublishReleaseAsync(opts),
+                    (ExportSubOptions opts) => ExportReleasesAsync(opts),
+                    (InitSubOptions opts) => CreateSampleConfigFileAsync(opts),
+                    (ShowConfigSubOptions opts) => ShowConfigAsync(opts),
+                    (LabelSubOptions opts) => CreateLabelsAsync(opts),
+                    errs => Task.FromResult(1)).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "{Message}", ex.Message);
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static void CreateFiglet(BaseSubOptions options)
@@ -96,198 +108,104 @@ namespace GitReleaseManager.Cli
 
         private static async Task<int> CreateReleaseAsync(CreateSubOptions subOptions)
         {
-            try
+            _vcsProvider = GetVcsProvider(subOptions);
+
+            Core.Model.Release release;
+            if (!string.IsNullOrEmpty(subOptions.Milestone))
             {
-                ConfigureLogging(subOptions.LogFilePath);
-
-                _vcsProvider = GetVcsProvider(subOptions);
-
-                Core.Model.Release release;
-                if (!string.IsNullOrEmpty(subOptions.Milestone))
+                var releaseName = subOptions.Name;
+                if (string.IsNullOrWhiteSpace(releaseName))
                 {
-                    var releaseName = subOptions.Name;
-                    if (string.IsNullOrWhiteSpace(releaseName))
-                    {
-                        releaseName = subOptions.Milestone;
-                    }
-
-                    release = await _vcsProvider.CreateReleaseFromMilestone(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone, releaseName, subOptions.TargetCommitish, subOptions.AssetPaths, subOptions.Prerelease).ConfigureAwait(false);
-                }
-                else
-                {
-                    release = await _vcsProvider.CreateReleaseFromInputFile(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Name, subOptions.InputFilePath, subOptions.TargetCommitish, subOptions.AssetPaths, subOptions.Prerelease).ConfigureAwait(false);
+                    releaseName = subOptions.Milestone;
                 }
 
-                Console.WriteLine(release.HtmlUrl);
-                return 0;
+                release = await _vcsProvider.CreateReleaseFromMilestone(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone, releaseName, subOptions.TargetCommitish, subOptions.AssetPaths, subOptions.Prerelease).ConfigureAwait(false);
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine(ex);
-
-                return 1;
+                release = await _vcsProvider.CreateReleaseFromInputFile(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Name, subOptions.InputFilePath, subOptions.TargetCommitish, subOptions.AssetPaths, subOptions.Prerelease).ConfigureAwait(false);
             }
+
+            Console.WriteLine(release.HtmlUrl);
+            return 0;
         }
 
         private static async Task<int> DiscardReleaseAsync(DiscardSubOptions subOptions)
         {
-            try
-            {
-                ConfigureLogging(subOptions.LogFilePath);
+            _vcsProvider = GetVcsProvider(subOptions);
 
-                _vcsProvider = GetVcsProvider(subOptions);
+            await _vcsProvider.DiscardRelease(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone);
 
-                await _vcsProvider.DiscardRelease(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone);
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-
-                return 1;
-            }
+            return 0;
         }
 
         private static async Task<int> AddAssetsAsync(AddAssetSubOptions subOptions)
         {
-            try
-            {
-                ConfigureLogging(subOptions.LogFilePath);
+            _vcsProvider = GetVcsProvider(subOptions);
 
-                _vcsProvider = GetVcsProvider(subOptions);
+            await _vcsProvider.AddAssets(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.TagName, subOptions.AssetPaths).ConfigureAwait(false);
 
-                await _vcsProvider.AddAssets(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.TagName, subOptions.AssetPaths).ConfigureAwait(false);
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-
-                return 1;
-            }
+            return 0;
         }
 
         private static async Task<int> CloseMilestoneAsync(CloseSubOptions subOptions)
         {
-            try
-            {
-                ConfigureLogging(subOptions.LogFilePath);
+            _vcsProvider = GetVcsProvider(subOptions);
 
-                _vcsProvider = GetVcsProvider(subOptions);
+            await _vcsProvider.CloseMilestone(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone).ConfigureAwait(false);
 
-                await _vcsProvider.CloseMilestone(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone).ConfigureAwait(false);
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-
-                return 1;
-            }
+            return 0;
         }
 
         private static async Task<int> OpenMilestoneAsync(OpenSubOptions subOptions)
         {
-            try
-            {
-                ConfigureLogging(subOptions.LogFilePath);
+            _vcsProvider = GetVcsProvider(subOptions);
 
-                _vcsProvider = GetVcsProvider(subOptions);
+            await _vcsProvider.OpenMilestone(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone).ConfigureAwait(false);
 
-                await _vcsProvider.OpenMilestone(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.Milestone).ConfigureAwait(false);
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-
-                return 1;
-            }
+            return 0;
         }
 
         private static async Task<int> PublishReleaseAsync(PublishSubOptions subOptions)
         {
-            try
-            {
-                ConfigureLogging(subOptions.LogFilePath);
+            _vcsProvider = GetVcsProvider(subOptions);
 
-                _vcsProvider = GetVcsProvider(subOptions);
-
-                await _vcsProvider.PublishRelease(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.TagName).ConfigureAwait(false);
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-
-                return 1;
-            }
+            await _vcsProvider.PublishRelease(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.TagName).ConfigureAwait(false);
+            return 0;
         }
 
         private static async Task<int> ExportReleasesAsync(ExportSubOptions subOptions)
         {
-            try
+            _vcsProvider = GetVcsProvider(subOptions);
+
+            var releasesMarkdown = await _vcsProvider.ExportReleases(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.TagName).ConfigureAwait(false);
+
+            using (var sw = new StreamWriter(File.Open(subOptions.FileOutputPath, FileMode.OpenOrCreate)))
             {
-                ConfigureLogging(subOptions.LogFilePath);
-
-                _vcsProvider = GetVcsProvider(subOptions);
-
-                var releasesMarkdown = await _vcsProvider.ExportReleases(subOptions.RepositoryOwner, subOptions.RepositoryName, subOptions.TagName).ConfigureAwait(false);
-
-                using (var sw = new StreamWriter(File.Open(subOptions.FileOutputPath, FileMode.OpenOrCreate)))
-                {
-                    sw.Write(releasesMarkdown);
-                }
-
-                return 0;
+                sw.Write(releasesMarkdown);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
 
-                return 1;
-            }
+            return 0;
         }
 
         private static Task<int> CreateSampleConfigFileAsync(InitSubOptions subOptions)
         {
-            ConfigureLogging(subOptions.LogFilePath);
-
             ConfigurationProvider.WriteSample(subOptions.TargetDirectory ?? Environment.CurrentDirectory, _fileSystem);
             return Task.FromResult(0);
         }
 
         private static Task<int> ShowConfigAsync(ShowConfigSubOptions subOptions)
         {
-            ConfigureLogging(subOptions.LogFilePath);
-
             Console.WriteLine(ConfigurationProvider.GetEffectiveConfigAsString(subOptions.TargetDirectory ?? Environment.CurrentDirectory, _fileSystem));
             return Task.FromResult(0);
         }
 
         private static async Task<int> CreateLabelsAsync(LabelSubOptions subOptions)
         {
-            try
-            {
-                ConfigureLogging(subOptions.LogFilePath);
+            _vcsProvider = GetVcsProvider(subOptions);
 
-                _vcsProvider = GetVcsProvider(subOptions);
-
-                await _vcsProvider.CreateLabels(subOptions.RepositoryOwner, subOptions.RepositoryName).ConfigureAwait(false);
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-
-                return 1;
-            }
+            await _vcsProvider.CreateLabels(subOptions.RepositoryOwner, subOptions.RepositoryName).ConfigureAwait(false);
+            return 0;
         }
 
         private static IVcsProvider GetVcsProvider(BaseVcsOptions subOptions)
@@ -298,47 +216,24 @@ namespace GitReleaseManager.Cli
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Not required.")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "This is required here.")]
-        private static void ConfigureLogging(string logFilePath)
+        private static void ConfigureLogging(BaseSubOptions options)
         {
-            var writeActions = new List<Action<string>>
-            {
-                s => _log.AppendLine(s)
-            };
+            string consoleTemplate = "[{Level:u3}] {Message:l}{NewLine}{Exception}";
 
-            if (!string.IsNullOrEmpty(logFilePath))
-            {
-                try
-                {
-                    Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
-                    if (File.Exists(logFilePath))
-                    {
-                        using (File.CreateText(logFilePath))
-                        {
-                        }
-                    }
+            var config = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Destructure.UsingAttributes()
+                .WriteTo.Console(outputTemplate: consoleTemplate,
+                                restrictedToMinimumLevel: LogEventLevel.Information,
+                                standardErrorFromLevel: LogEventLevel.Warning,
+                                theme: AnsiConsoleTheme.Code);
 
-                    writeActions.Add(x => WriteLogEntry(logFilePath, x));
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Failed to configure logging: " + ex.Message);
-                }
-            }
-            else
+            if (!string.IsNullOrEmpty(options.LogFilePath))
             {
-                // if nothing else is specified, write to console
-                writeActions.Add(Console.WriteLine);
+                config = config.WriteTo.File(options.LogFilePath, encoding: new UTF8Encoding(false));
             }
 
-            Logger.WriteInfo = s => writeActions.ForEach(a => a(s));
-            Logger.WriteWarning = s => writeActions.ForEach(a => a(s));
-            Logger.WriteError = s => writeActions.ForEach(a => a(s));
-        }
-
-        private static void WriteLogEntry(string logFilePath, string s)
-        {
-            var contents = string.Format(CultureInfo.InvariantCulture, "{0}\t\t{1}\r\n", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture), s);
-            File.AppendAllText(logFilePath, contents);
+            Log.Logger = config.CreateLogger();
         }
     }
 }
