e4bee05b627649c150fa67c9689d4060ca1ac9e2
AdmiringWorm <kim.nordmo@gmail.com>
2020-09-16 06:04:47 +0200

    (GH-217) Add option to extract embedded templates

--- src/GitReleaseManager.Core/Commands/InitCommand.cs
+++ src/GitReleaseManager.Core/Commands/InitCommand.cs
@@ -7,10 +7,12 @@
 namespace GitReleaseManager.Core.Commands
 {
     using System;
+    using System.IO;
     using System.Threading.Tasks;
     using GitReleaseManager.Core.Configuration;
     using GitReleaseManager.Core.Helpers;
     using GitReleaseManager.Core.Options;
+    using GitReleaseManager.Core.Templates;
     using Serilog;
 
     public class InitCommand : ICommand<InitSubOptions>
@@ -26,9 +28,46 @@ namespace GitReleaseManager.Core.Commands
 
         public Task<int> Execute(InitSubOptions options)
         {
-            _logger.Information("Creating sample configuration file");
             var directory = options.TargetDirectory ?? Environment.CurrentDirectory;
-            ConfigurationProvider.WriteSample(directory, _fileSystem);
+
+            if (!options.ExtractTemplates)
+            {
+                _logger.Information("Creating sample configuration file");
+                ConfigurationProvider.WriteSample(directory, _fileSystem);
+            }
+            else
+            {
+                _logger.Information("Extracting Embedded templates");
+
+                var templates = ReleaseTemplates.GetExportTemplates();
+                var config = ConfigurationProvider.Provide(directory, _fileSystem);
+                var templatesDirectory = new DirectoryInfo(_fileSystem.ResolvePath(config.TemplatesDirectory));
+
+                if (!templatesDirectory.Exists)
+                {
+                    templatesDirectory.Create();
+                }
+
+                foreach (var template in templates)
+                {
+                    var fullFilePath = Path.Combine(templatesDirectory.FullName, template.Key);
+                    var templateDir = Path.GetDirectoryName(fullFilePath);
+                    if (!Directory.Exists(templateDir))
+                    {
+                        Directory.CreateDirectory(templateDir);
+                    }
+                    else if (File.Exists(fullFilePath))
+                    {
+                        _logger.Warning("File '{FilePath}' already exist. Skipping file.", template.Key);
+                        continue;
+                    }
+
+                    _logger.Information("Creating new file '{FilePath}'!", template.Key);
+                    _fileSystem.WriteAllText(fullFilePath, template.Value);
+                }
+
+                _logger.Information("All embedded templates has been extracted!");
+            }
 
             return Task.FromResult(0);
         }
--- src/GitReleaseManager.Core/Options/InitSubOptions.cs
+++ src/GitReleaseManager.Core/Options/InitSubOptions.cs
@@ -11,5 +11,7 @@ namespace GitReleaseManager.Core.Options
     [Verb("init", HelpText = "Creates a sample Yaml Configuration file in root directory")]
     public class InitSubOptions : BaseSubOptions
     {
+        [Option("templates", HelpText = "Extract the embedded templates to allow modification", Required = false)]
+        public bool ExtractTemplates { get; set; }
     }
 }
