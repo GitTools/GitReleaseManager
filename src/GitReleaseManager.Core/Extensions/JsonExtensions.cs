e6a02c400ea0aeb45dba2dec1fe89823bfff7b05
jericho <desautelsj@hotmail.com>
2024-07-19 13:40:03 -0400

    Convenient extension method to search for a node in a json doc that corresponds to a certain path. If the node is not present in the doc, fallback to a second path. If this second path is not present, fallback to a third path and so on.

--- src/GitReleaseManager.Core/Extensions/JsonExtensions.cs
+++ src/GitReleaseManager.Core/Extensions/JsonExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 
@@ -6,6 +7,12 @@ namespace GitReleaseManager.Core.Extensions
 {
     internal static class JsonExtensions
     {
+        /// <summary>
+        /// Get a JsonElement from a path. Each level in the path is seperated by a dot.
+        /// </summary>
+        /// <param name="jsonElement">The parent Json element.</param>
+        /// <param name="path">The path of the desired child element.</param>
+        /// <returns>The child element.</returns>
         public static JsonElement GetJsonElement(this JsonElement jsonElement, string path)
         {
             if (jsonElement.ValueKind is JsonValueKind.Null || jsonElement.ValueKind is JsonValueKind.Undefined)
@@ -37,6 +44,36 @@ namespace GitReleaseManager.Core.Extensions
             }
 
             return jsonElement;
+        }
+
+        /// <summary>
+        /// Get the first JsonElement matching a path from the provided list of paths.
+        /// </summary>
+        /// <param name="jsonElement">The parent Json element.</param>
+        /// <param name="paths">The path of the desired child element.</param>
+        /// <returns>The child element.</returns>
+        public static JsonElement GetFirstJsonElement(this JsonElement jsonElement, IEnumerable<string> paths)
+        {
+            if (jsonElement.ValueKind is JsonValueKind.Null || jsonElement.ValueKind is JsonValueKind.Undefined)
+            {
+                return default(JsonElement);
+            }
+
+            var element = default(JsonElement);
+
+            foreach (var path in paths)
+            {
+                element = jsonElement.GetJsonElement(path);
+
+                if (element.ValueKind is JsonValueKind.Null || element.ValueKind is JsonValueKind.Undefined)
+                {
+                    continue;
+                }
+
+                break;
+            }
+
+            return element;
         }
 
         public static string GetJsonElementValue(this JsonElement jsonElement) => jsonElement.ValueKind != JsonValueKind.Null &&
